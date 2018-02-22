779e49aca5a0e2399e8cddd457bc7a9a7ff19fdc
Itzchuck <ieshachnovitz@gmail.com>
2018-02-22 11:19:01 +0200

    Delegate event listener

--- script.js
+++ script.js
@@ -2,52 +2,28 @@ var workingNumbers = document.querySelector('.working-numbers')
 var result = document.querySelector('.result')
 var allClear = document.querySelector('.ac')
 var clear = document.querySelector('.ce')
-var one = document.querySelector('.one')
-var two = document.querySelector('.two')
-var three = document.querySelector('.three')
-var four = document.querySelector('.four')
-var five = document.querySelector('.five')
-var six = document.querySelector('.six')
-var seven = document.querySelector('.seven')
-var eight = document.querySelector('.eight')
-var nine = document.querySelector('.nine')
-var zero = document.querySelector('.zero')
-var plus = document.querySelector('.plus')
-var minus = document.querySelector('.minus')
-var divide = document.querySelector('.divide')
-var multiply = document.querySelector('.multiply')
 var equals = document.querySelector('.equals')
-var point = document.querySelector('.point')
 
-one.addEventListener('click', displayBtnPress)
-two.addEventListener('click', displayBtnPress)
-three.addEventListener('click', displayBtnPress)
-four.addEventListener('click', displayBtnPress)
-five.addEventListener('click', displayBtnPress)
-six.addEventListener('click', displayBtnPress)
-seven.addEventListener('click', displayBtnPress)
-eight.addEventListener('click', displayBtnPress)
-nine.addEventListener('click', displayBtnPress)
-zero.addEventListener('click', displayBtnPress)
-plus.addEventListener('click', displayBtnPress)
-minus.addEventListener('click', displayBtnPress)
-multiply.addEventListener('click', displayBtnPress)
-divide.addEventListener('click', displayBtnPress)
-point.addEventListener('click', displayBtnPress)
+var buttonsContainer = document.body.querySelector('.buttons');
+
+buttonsContainer.addEventListener('click', displayBtnPress)
 
 allClear.addEventListener('click', clearAll)
 clear.addEventListener('click', deleteInput)
 equals.addEventListener('click', resultEquals)
 var numbersArr = []
 
-function displayBtnPress() {
-    if (workingNumbers.innerHTML == '0'){
-        workingNumbers.innerHTML = this.value;
-        numbersArr.push(this.value)
+function displayBtnPress(e) {
+    if (e.target.value == undefined){
+        console.log('here second');
+        return
+    }else if (workingNumbers.innerHTML == '0'){
+        workingNumbers.innerHTML = e.target.value;
+        numbersArr.push(e.target.value)
     }
     else{
-        workingNumbers.innerHTML += this.value;
-        numbersArr.push(this.value)
+        workingNumbers.innerHTML += e.target.value;
+        numbersArr.push(e.target.value)
     }
 }
 
@@ -60,5 +36,7 @@ function deleteInput() {
 }
 
 function resultEquals(){
-    result.innerHTML = eval(workingNumbers.innerHTML)
+    console.log('here first');
+    result.innerHTML = eval(workingNumbers.innerHTML);
+    workingNumbers.innerHTML = '0';
 }
